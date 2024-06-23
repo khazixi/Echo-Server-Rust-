969676fa716317cea338caa607d342cc565b085a
Khazixi <khazixfangirl@gmail.com>
2024-06-22 21:43:43 -0400

    Client messages done

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::net;
+use std::io::{Write, Read};
+fn main() -> std::io::Result<()> {
+    let mut connection = net::TcpStream::connect("127.0.0.1:42069")?;
+
+    let mut input_handle = std::io::stdin();
+
+    let mut buf = [0u8; 8192];
+    
+    loop {
+        input_handle.read(&mut buf)?;
+
+        if buf.starts_with(b"quit") {
+            println!("Exiting");
+            break;
+        }
+
+        connection.write(&buf)?;
+
+        connection.flush()?;
+    }
+
+    Ok(())
 }
