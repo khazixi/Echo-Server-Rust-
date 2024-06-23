54131ffc68fdc9782c9fbb1bf3249f976f2ac01a
Khazixi <khazixfangirl@gmail.com>
2024-06-22 21:43:30 -0400

    Got Looping on the server working

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,25 +1,33 @@
 use std::{
-    fmt::write,
-    io::{self, stdout, Read, Write, Stdout},
-    net::{self, TcpListener, TcpStream},
+    io::{self, Read, Write},
+    net::{self},
 };
+
+fn handle_connection(
+    echo_stream: &mut net::TcpStream,
+    output_stream: &mut io::StdoutLock,
+) -> io::Result<()> {
+    let mut buf: [u8; 8192] = [0; 8192];
+
+    loop {
+        echo_stream.read(&mut buf);
+
+        // echo_stream.write(&buf);
+
+        output_stream.write(&buf);
+
+        echo_stream.flush();
+    }
+    Ok(())
+}
 
 fn main() -> std::io::Result<()> {
     let listener = net::TcpListener::bind("127.0.0.1:42069")?;
 
     let mut stdout_handle = io::stdout().lock();
 
-    let mut buf: [u8; 8192] = [0; 8192];
-
     for stream in listener.incoming() {
-        let bytes_read = stream?.read(&buf)?;
-
-        stdout_handle.write(&buf);
-
-        stream?.write(&buf);
-
-        stdout_handle.flush();
-        stream?.flush();
+        handle_connection(&mut stream?, &mut stdout_handle);
     }
 
     Ok(())
